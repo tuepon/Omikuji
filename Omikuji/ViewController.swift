f940e77b7373d764d02e3ca05267ba09cdb28359
Tomonori Ueno <t.uepon@gmail.com>
2021-03-09 17:45:23 +0700

    Created app without images

--- Omikuji/ViewController.swift
+++ Omikuji/ViewController.swift
@@ -8,12 +8,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var luckyLabel: UILabel!
+    // First, what to do?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Hyde label
+        self.luckyLabel.isHidden = true
     }
 
-
+    @IBAction func tapButton(_ sender: Any) {
+        // Show label
+        self.luckyLabel.isHidden = false
+        
+        // Get the random number
+        let random = arc4random()%5
+        
+        // random 0 Best luck
+        // random 1 Fair luck
+        // random 2 A little luck
+        // random 3 Bad luck
+        // random 4 Worst luck
+        if random == 0 {
+            self.luckyLabel.text = "Best luck"
+        } else if random == 1 {
+            self.luckyLabel.text = "Fair luck"
+        } else if random == 2 {
+            self.luckyLabel.text = "A little luck"
+        } else if random == 3 {
+            self.luckyLabel.text = "Bad luck"
+        } else {
+            self.luckyLabel.text = "Worst luck"
+        }
+        
+    }
+    
 }
 
